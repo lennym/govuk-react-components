356f3201b70f6758e1015e2cd8bb403440a5da98
Leonard Martin <leonard.martin@gmail.com>
2018-01-11 16:35:49 +0000

    Make phase banner configurable
    If a feedback url is provided then use the standard text, otherwise allow the entire content to be passed as either a string or another react component.

--- src/phase-banner.jsx
+++ src/phase-banner.jsx
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 
 class PhaseBanner extends React.Component {
 
+  renderContent() {
+    if (this.props.content) {
+      return this.props.content
+    } else if (this.props.feedbackUrl) {
+      return <span>This is a new service – your <a href={this.props.feedbackUrl}>feedback</a> will help us to improve it.</span>
+    }
+  }
+
   render() {
     if (!this.props.phase) {
       return;
@@ -11,7 +19,7 @@ class PhaseBanner extends React.Component {
     return <div className="phase-banner">
       <p>
         <strong className="phase-tag">{this.props.phase}</strong>
-        <span>This is a new service – your <a href="#">feedback</a> will help us to improve it.</span>
+        { this.renderContent() }
       </p>
     </div>
 
@@ -19,8 +27,17 @@ class PhaseBanner extends React.Component {
 
 }
 
+
+
+
+PhaseBanner.defaultProps = {
+  phase: 'prototype'
+};
+
 PhaseBanner.propTypes = {
-  phase: PropTypes.oneOf(['prototype', 'alpha', 'beta'])
+  phase: PropTypes.oneOf(['prototype', 'alpha', 'beta']),
+  feedbackUrl: PropTypes.string,
+  content: PropTypes.node
 };
 
 export default PhaseBanner;
