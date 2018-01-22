71594986066e6281b7cdc9f61ebe57d8178d2e1b
James Gee <James.gee@digital.homeoffice.gov.uk>
2018-01-22 16:40:39 +0000

    Semi-colons too

--- src/button/index.jsx
+++ src/button/index.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types';
 export const buttonOf = (Type) => {
   const component = (props) => (
     <Type {...props} className={`button${props.className ? ' ' + props.className : ''}`}>{props.children}</Type>
-  )
+  );
   component.defaultName = 'ButtonOfType' + Type
   component.propTypes = {
     children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
     onClick: PropTypes.func,
     disabled: PropTypes.bool,
     className: PropTypes.string
-  }
+  };
   return component;
-}
+};
 
 export const Button = buttonOf('button');
 Button.propTypes = Object.assign({ type: PropTypes.string }, Button.propTypes);
